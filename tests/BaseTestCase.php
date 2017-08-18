abfc02e65b4e271fbc06f5dc000ed3f9dffa9b93
Christopher Francisco <cfrancisco@cdspanama.org>
2017-08-18 10:01:44 -0500

    Add base test class and test script command

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class: BaseTestCase
+ *
+ * @see TestCase
+ * @final
+ */
+final class BaseTestCase extends TestCase
+{
+    public function testItWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
